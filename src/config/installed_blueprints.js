26277037a1988c5284df30bab4d7d2a622a94b26
Crowdbotics <team@crowdbotics.com>
2020-09-26 10:34:39 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'Maps111337', human_name: 'Maps', access_route: 'Maps111337', icon: 'map'},
 { name: 'ArticleList111314', human_name: 'Article List', access_route: 'ArticleList111314'},
 { name: 'Maps111301', human_name: 'Maps', access_route: 'Maps111301', icon: 'map'},
-{ name: 'BlankScreen1111296', human_name: 'Blank Screen 1', access_route: 'BlankScreen1111296'},
 { name: 'BlankScreen0111295', human_name: 'Blank Screen 0', access_route: 'BlankScreen0111295'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import ArticleList111350Navigator from '../features/ArticleList111350/navigator'
 import Maps111337Navigator from '../features/Maps111337/navigator';
 import ArticleList111314Navigator from '../features/ArticleList111314/navigator';
 import Maps111301Navigator from '../features/Maps111301/navigator';
-import BlankScreen1111296Navigator from '../features/BlankScreen1111296/navigator';
 import BlankScreen0111295Navigator from '../features/BlankScreen0111295/navigator';
 
 /**
@@ -25,7 +24,6 @@ ArticleList111350: { screen: ArticleList111350Navigator },
 Maps111337: { screen: Maps111337Navigator },
 ArticleList111314: { screen: ArticleList111314Navigator },
 Maps111301: { screen: Maps111301Navigator },
-BlankScreen1111296: { screen: BlankScreen1111296Navigator },
 BlankScreen0111295: { screen: BlankScreen0111295Navigator },
 
     /** new navigators can be added here */
